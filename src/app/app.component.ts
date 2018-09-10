28a7fc11649d4eb32476ba2e579b1fad60712ac6
wizzard1994 <matik550@gmail.com>
2018-09-10 14:51:21 +0200

    After directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cooking-app';
+  loadedFeature = 'recipe';
+  selectFeature(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
