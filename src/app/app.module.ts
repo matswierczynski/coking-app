fb2718debd525a73560a15fb859e2e6c75d515c2
wizzard1994 <matik550@gmail.com>
2018-09-19 09:24:36 +0200

    Services moved from app.module to core.module, AuthGuardService moved to lazy loading module: RecipesModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpModule} from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
-import {AppRoutingModule} from './app-routing.module';
-import {RecipeDetailResolverService} from './recipies/recipe-detail/recipe-detail-resolver.service';
-import {RecipeService} from './recipies/recipe.service';
-import {DataStorageService} from './shared/data-storage.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuardService} from './auth/auth-guard.service';
-import {SharedModule} from './shared/shared.module';
-import {ShoppingModule} from './shopping-list/shopping.module';
-import {AuthModule} from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingModule } from './shopping-list/shopping.module';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,10 +20,9 @@ import { HomeComponent } from './home/home.component';
     SharedModule,
     ShoppingModule,
     AuthModule,
+    CoreModule,
     AppRoutingModule,
   ],
-  providers: [ShoppingListService, RecipeDetailResolverService, RecipeService, DataStorageService,
-  AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
