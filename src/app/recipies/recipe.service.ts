58e6b1fc3ec60d904858b5b5eb3484e1df0e0e04
wizzard1994 <matik550@gmail.com>
2018-09-24 15:09:44 +0200

    RecipeService replaced with ngrx Store & Actions

--- src/app/recipies/recipe.service.ts
+++ src/app/recipies/recipe.service.ts
@@ -1,30 +1,12 @@
 import {Recipe} from './recipe.model';
 import { Injectable} from '@angular/core';
-import {Ingredient} from '../shared/ingredient.model';
 import {Subject} from 'rxjs';
 
 @Injectable()
 export class RecipeService {
   recipe: Recipe;
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super tasty Schnitzel',
-      'https://thestayathomechef.com/wp-content/uploads/2018/03/German-Schnitzel-3-small.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe(
-      'Big Fat Burger',
-      'A super fat Burger',
-      'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ])
-  ];
+  recipes: Recipe [];
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
---
+++ src/app/recipies/store/recipe.reducers.ts
@@ -0,0 +1,67 @@
+import {Recipe} from '../recipe.model';
+import {Ingredient} from '../../shared/ingredient.model';
+import * as RecipeActions from './recipe.actions';
+
+export interface FeatureState {
+  recipes: State;
+}
+export interface State {
+  recipes: Recipe[];
+}
+
+const initialState: State = {
+  recipes: [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel',
+      'https://thestayathomechef.com/wp-content/uploads/2018/03/German-Schnitzel-3-small.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'A super fat Burger',
+      'https://www.seriouseats.com/recipes/images/2015/07/20150702-sous-vide-hamburger-anova-primary.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ]
+};
+export function recipeReducers(state, action: RecipeActions.RecipeActions) {
+  switch (action.type) {
+    case (RecipeActions.SET_RECIPES):
+      return {
+        ...state,
+        recipes: [...action.payload]
+      };
+    case (RecipeActions.ADD_RECIPE):
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload]
+      };
+    case (RecipeActions.UPDATE_RECIPE):
+      const recipe = state.recipes[action.payload.index];
+      const updatedRecipe = {
+        ...recipe,
+        ...action.payload.updatedRecipe
+      };
+      const recipes = [...state.recipes];
+      recipes[action.payload.index] = updatedRecipe;
+      return {
+        ...state,
+        recipes: recipes
+      };
+    case (RecipeActions.DELETE_RECIPE):
+      const oldRecipes = [...state.recipes];
+      oldRecipes.splice(action.payload, 1);
+      return {
+        ...state,
+        recipes: oldRecipes
+      };
+    default: {
+      return state;
+    }
+  }
+}
